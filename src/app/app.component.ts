82e7109300093ac0ecef0cdd64a89e5b5279233f
oomusou <oomusou@gmail.com>
2017-05-21 19:27:09 +0800

    app.component.ts 新增 animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { trigger, state, style } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('signal', [
+      state('go', style({
+        'background-color': 'green'
+      }))
+    ])
+  ]
 })
 export class AppComponent {
 }
